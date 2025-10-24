b5b0d3146b29ab21f72dd5ee8f3615832ef3d222
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-10-24 10:57:38 +0800

    refactor

--- src/components/Map3D.tsx
+++ src/components/Map3D.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
 import DeckGL from "@deck.gl/react";
 import { ScatterplotLayer } from "@deck.gl/layers";
 import { Map } from "react-map-gl/mapbox";
 
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+const DATA_URL = "/data/earthquakes.json";
 
 interface EarthquakeData {
     id: string;
@@ -18,52 +18,35 @@ interface EarthquakeData {
 }
 
 export default function Map3D() {
-    const [data, setData] = useState<EarthquakeData[]>([]);
-
-    useEffect(() => {
-        fetch("https://raw.githubusercontent.com/GReturn/phivolcs-earthquake-data-scraper/refs/heads/main/data/phivolcs_earthquake_all_years.csv")
-        .then((response) => response.text())
-        .then((csvText) => {
-            const rows = csvText.split("\n").slice(1, 1001); // TODO edit this later when done tweaking shit; should be .slice(1)
-            const parsedData: EarthquakeData[] = rows.map((row) => {
-                const [id, datetime, latitude, longitude, depth, magnitude, location, month, year] = row.split(",");
-                return {
-                    id,
-                    datetime,
-                    latitude: parseFloat(latitude),
-                    longitude: parseFloat(longitude),
-                    depth: parseFloat(depth),
-                    magnitude: parseFloat(magnitude),
-                    location,
-                    month,
-                    year: parseInt(year)
-                }
-            }).filter((d) => !isNaN(d.latitude) && !isNaN(d.longitude) && !isNaN(d.magnitude));
-            setData(parsedData);
-        })
-    }, []);
-
-    const layers = new ScatterplotLayer({
+    // TODO change the visuals
+    const layers = new ScatterplotLayer<EarthquakeData>({
         id: "earthquakes",
-        data,
-        getPosition: (d: EarthquakeData) => [d.longitude, d.latitude, -d.depth],
-        getRadius: (d: EarthquakeData) => d.magnitude * 100,
-        getFillColor: (d: EarthquakeData) => [255, 128 - d.depth, 0, 150],
+        data: DATA_URL,
+        getPosition: d => [d.longitude, d.latitude, -d.depth * 1000],
+        getRadius: d => Math.pow(1.5, d.magnitude) * 100,
+        getFillColor: d => [255, 255 - d.depth * 2, 0, 180],
         radiusMinPixels: 2,
+        radiusMaxPixels: 20,
         pickable: true,
+        autoHighlight: true,
     });
 
     return (
         <DeckGL
             initialViewState={{
                 longitude: 122,
-                latitude: 12,
-                zoom: 4,
-                pitch: 40,
+                latitude: 12.5,
+                zoom: 5.5,
+                pitch: 50,
                 bearing: 0
             }}
             controller={true}
             layers={[layers]}
+            getTooltip={({object}) => object && (
+                `Mag ${object.magnitude} Earthquake\n`+
+                `Location: ${object.location}\n`+
+                `Depth: ${object.depth} km\n`
+            )}
             >
                 <Map
                     mapboxAccessToken={MAPBOX_TOKEN}
